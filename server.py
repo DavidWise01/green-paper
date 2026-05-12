49783779a7c1b90ae997919bc6e4b00648a558d1
DavidWise01 <148925959+DavidWise01@users.noreply.github.com>
2026-05-11 23:53:04 -0500

    add canary and persistent /data

--- server.py
+++ server.py
@@ -25,7 +25,11 @@ def get_db():
 
 @app.route("/")
 def home():
-    return send_from_directory(".", "index.html")
+    response = send_from_directory(".", "index.html")
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
 
 @app.route("/shout", methods=["POST"])
 def shout():
